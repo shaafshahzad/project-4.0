2b425b640b49e96f3467b9ce5f9b0dd227e9cb94
Shaaf <shaaf.m.shahzad@gmail.com>
2024-06-18 04:50:30 -0400

    Build issues pt2

--- components/dashboard/widgets/courses/courses.tsx
+++ components/dashboard/widgets/courses/courses.tsx
@@ -1,10 +1,10 @@
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import React from "react";
 import CourseList from "./course-list";
@@ -12,25 +12,25 @@ import PdfUpload from "./pdf-upload";
 import AddCourse from "./add-course";
 
 const Courses = () => {
-	return (
-		<Card className="col-span-2 flex flex-col">
-			<CardHeader className="flex flex-row justify-between">
-				<div className="w-[85%]">
-					<CardTitle>Your courses</CardTitle>
-					<CardDescription>
-						Upload your course outlines or add them manually
-					</CardDescription>
-				</div>
-				<AddCourse />
-			</CardHeader>
-			<CardContent className="h-full overflow-y-auto">
-				<CourseList />
-			</CardContent>
-			<CardFooter className="pt-2">
-				<PdfUpload />
-			</CardFooter>
-		</Card>
-	);
+  return (
+    <Card className="col-span-2 flex flex-col">
+      <CardHeader className="flex flex-row justify-between">
+        <div className="w-[85%]">
+          <CardTitle>Your courses</CardTitle>
+          <CardDescription>
+            Upload your course outlines or add them manually
+          </CardDescription>
+        </div>
+        <AddCourse pathname={"/dashboard"} />
+      </CardHeader>
+      <CardContent className="h-full overflow-y-auto">
+        <CourseList />
+      </CardContent>
+      <CardFooter className="pt-2">
+        <PdfUpload />
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default Courses;
